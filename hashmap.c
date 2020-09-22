534ff726c8e6cf82c5a862a1ed20a0cc2bf45ba1
repl.it user <replituser@example.com>
2020-09-22 23:47:45 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -121,7 +121,7 @@ void * firstMap(HashMap * map) {
 
   for (i = 0 ; i < map->capacity ; i++)
   {
-    if (map->buckets[i] != NULL)
+    if (map->buckets[i] != NULL && map->buckets[i]->value != NULL)
     {
       return map->buckets[i]->value;
     }
